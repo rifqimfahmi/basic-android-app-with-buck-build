7b0f5bae2e5f52df9c1ed62ac73db864f543fe4e
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-06-30 15:11:20 +0700

    Show Toast on the activity

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
   }
 }
