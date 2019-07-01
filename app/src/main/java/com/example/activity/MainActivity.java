cddccdfd9350f38757d5497d2da4189ee8873780
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-07-01 10:42:10 +0700

    Add jar and aar to BUCK file. Dispatchers Main still missing

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.util.Log;
+
+import kotlinx.coroutines.Dispatchers;
+import kotlinx.coroutines.MainCoroutineDispatcher;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
+
+    MainCoroutineDispatcher dispatcher = Dispatchers.getMain();
+    Log.d("SOME_DISPATCHERS", dispatcher.toString());
   }
 }
